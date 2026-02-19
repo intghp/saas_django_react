e103611b7ef5a991fe5e5bcf7009de48995a76a3
intghp <gustavohlucenapaiva1@gmail.com>
2026-02-19 09:04:03 -0300

    feat: add backend authentication

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
+    'apps.accounts',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +52,19 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
+
+DJOSER = {
+    'USER_ID_FIELD': 'username'
+}
 
 ROOT_URLCONF = 'config.urls'
 
